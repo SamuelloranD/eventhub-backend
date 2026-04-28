83dea853e50a883e7beb98dc2de13e6c559f2b87
Samuel Lorand <samuel.lorand@hotmail.com>
2026-04-28 17:53:41 -0300

    feat: adiciona endpoint /test para validação da API

---
+++ src/main/java/eventhub/eventhub/controller/TestController.java
@@ -0,0 +1,15 @@
+package eventhub.eventhub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String teste() {
+        return "API rodando";
+    }
+}
